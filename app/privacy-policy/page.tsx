43d763c3c364ba45fafdf1a663f08d9d6a214590
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-12-21 20:02:34 -0500

    Fixed apostrophe

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -107,7 +107,7 @@ export const metadata = {
         </ul>
         <h4 className="text-lg font-medium mb-2">Usage Data</h4>
         <p className="mb-4">
-          Usage Data is collected automatically when using the Service. It may include information such as Your Device's
+          Usage Data is collected automatically when using the Service. It may include information such as Your Device&#39;s
           Internet Protocol address (e.g., IP address), browser type, browser version, the pages of our Service that You
           visit, the time and date of Your visit, the time spent on those pages, unique device identifiers, and other
           diagnostic data.
